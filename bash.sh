35b4d51f0272ebc4f9a583386c11cc3069f68c4e
Миша Курашев <kurashevmichael@gmail.com>
2024-09-26 17:38:33 +0300

    version for deploy

--- bash.sh
+++ bash.sh
@@ -3,9 +3,9 @@
 export PGPASSWORD='postgres'
 script_dir=$(dirname "$0")
 
-#psql -h db_new -U postgres -d sqlalchemy_tuts -f "$script_dir/fixtures/tariff.sql"
-#psql -h db_new -U postgres -d sqlalchemy_tuts -f "$script_dir/fixtures/ai_model.sql"
-psql -h localhost -U postgres -d sqlalchemy_tuts -f "$script_dir/fixtures/tariff.sql"
-psql -h localhost -U postgres -d sqlalchemy_tuts -f "$script_dir/fixtures/ai_model.sql"
+psql -h db_new -U postgres -d sqlalchemy_tuts -f "$script_dir/fixtures/tariff.sql"
+psql -h db_new -U postgres -d sqlalchemy_tuts -f "$script_dir/fixtures/ai_model.sql"
+#psql -h localhost -U postgres -d sqlalchemy_tuts -f "$script_dir/fixtures/tariff.sql"
+#psql -h localhost -U postgres -d sqlalchemy_tuts -f "$script_dir/fixtures/ai_model.sql"
 
 unset PGPASSWORD
