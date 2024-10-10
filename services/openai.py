4024a45f4e4f98a0608f6f93351979fd6a9f8fcd
Миша Курашев <kurashevmichael@gmail.com>
2024-10-10 21:32:05 +0300

    afasfafafasf

--- services/openai.py
+++ services/openai.py
@@ -6,7 +6,7 @@ class ChatGPT:
     def __init__(self, token: str, base_url: str, token_not_off: str, base_url_not_off, proxy_url: str):
         # self.client_chat_gpt = OpenAI(api_key=token, base_url=base_url)
         limits = httpx.Limits(max_connections=None, max_keepalive_connections=None)
-        self.async_client_chat_gpt = AsyncOpenAI(api_key=token, base_url=base_url)
+        self.async_client_chat_gpt = AsyncOpenAI(api_key=token, base_url=base_url, http_client=httpx.AsyncClient(proxies=proxy_url, limits=limits))
         self.async_not_official_client_chat_gpt = AsyncOpenAI(api_key=token_not_off, base_url=base_url_not_off, http_client=httpx.AsyncClient(proxies=proxy_url, limits=limits))
 
     async def async_generate_text(self, ai_model: str, context: list, prompt: str):
