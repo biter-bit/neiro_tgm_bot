9fba9c3f8a33ac7d394b73b613b4448b0789a061
Миша Курашев <kurashevmichael@gmail.com>
2024-10-15 16:12:02 +0300

    update logger

--- services/logger_service.py
+++ services/logger_service.py
@@ -1,14 +1,31 @@
 import logging
 from logging import Logger
+from config import settings
+import os
 
 
 def create_logger(level_logger: str) -> Logger:
     """Создай логер"""
+    basic_path = os.path.join(settings.PATH_WORK, 'logs')
+    print(basic_path)
+
     log_format = "%(asctime)s [%(levelname)s] [%(name)s] [%(threadName)s] [%(filename)s:%(lineno)d] - %(message)s"
 
     logging.basicConfig(level=getattr(logging, level_logger.upper()), format=log_format)
 
     logger = logging.getLogger("my_logger")
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(getattr(logging, level_logger.upper()))
+
+    # Создание файлового обработчика для обычных логов
+    file_handler = logging.FileHandler(os.path.join(basic_path, 'app.log'))
+    file_handler.setLevel(logging.INFO)  # Логи от INFO и выше
+    file_handler.setFormatter(logging.Formatter(log_format))
+    logger.addHandler(file_handler)
+
+    # Создание файлового обработчика для ошибок
+    error_handler = logging.FileHandler(os.path.join(basic_path, 'errors.log'))
+    error_handler.setLevel(logging.ERROR)  # Логи только для ошибок
+    error_handler.setFormatter(logging.Formatter(log_format))
+    logger.addHandler(error_handler)
 
     return logger
